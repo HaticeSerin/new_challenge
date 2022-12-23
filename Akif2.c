09db2fa5866eb09acdbcdacf3403eaeddb115e4f
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-23 19:02:52 +0300

    Hatice Serin
    @themanoftalent

---
+++ Akif2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+int i,j,k,n;
+printf("Enter tne number of rows :");
+scanf("%d", &n);
+
+for(i=1; i<=n; i++)
+{
+    for(j=i; j<=n; j++)
+    {
+        printf("  ");
+    }
+    for(k=1; k<=i*2-1; k++)
+    {
+        printf("Akif");
+    }
+    printf("\n");
+}    
+    
+    return 0;
+}
